23e7858b258a3e0d287686ceb99c5b9b7cde5aee
FaiberCamachoDev <faibercamacho16@gmail.com>
2026-04-24 17:01:31 -0500

    feat: add task 1 week 4

--- models/Animal.cs
+++ models/Animal.cs
@@ -1,8 +1,14 @@
-using System;
+/* * DECISIONES DE DISEÑO - SEMANA 4:
+ * * 1. Clases Abstractas: Se usan para 'VeterinaryService' y 'Animal' porque 
+ * comparten una identidad base y propiedades comunes (ej. Cost, Name). Define "qué ES" el objeto.
+ * * 2. Interfaces: Se usarán 'IRegistrable', 'IAtendible' e 'INotificable' porque 
+ * definen "qué PUEDE HACER" el objeto. Esto nos da flexibilidad: un Paciente y una Mascota 
+ * no comparten la misma clase base, pero ambos comparten la capacidad de ser registrados (IRegistrable).
+ */
 
 namespace models;
 
-public class Animal
+public abstract class Animal
 {
     public string Name { get; set; }
     public int Age { get; set; }
